a31002ffe7cbdc1bc812d13cd2d07a42c26138ad
Ajay Kumar Yadav <96533388+Ajay1105@users.noreply.github.com>
2023-10-12 22:02:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Duplicate subtree in Binary Tree/duplicate-subtree-in-binary-tree.cpp
@@ -0,0 +1,146 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	char data;
+	struct Node *left;
+	struct Node *right;
+
+	Node(char x) {
+		data = x;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+struct Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node *root = new Node(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current Node
+			currNode->left = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+// } Driver Code Ends
+/*The structure of the Binary Tree Node  is
+struct Node
+{
+  char data;
+  struct Node* left;
+  struct Node* right;
+};*/
+
+class Solution {
+  public:
+    
+    unordered_set<string> subtrees;
+    char marker = '!'; 
+    
+    string checkDub(Node*root){
+        string s = "";
+        if(root == NULL) return s+marker;
+        
+        string lstr = checkDub(root->left);
+        if(lstr=="") return s;
+        
+        string rstr = checkDub(root->right);
+        if(rstr=="") return s;
+        
+        s= s + root->data+ lstr +rstr;
+        
+        if (s.length() > 3 and 
+            subtrees.find(s) != subtrees.end())
+           return "";
+     
+        subtrees.insert(s);
+     
+        return s;
+    }
+    
+    int dupSub(Node *root) {
+         string str = checkDub(root);
+         if(str.compare("") == 0)
+            return 1;
+        else
+            return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	
+	int t;
+	cin >> t;
+	//cout << t << "\n";
+	while (t--)
+	{
+		string treeString;
+		getline(cin >> ws, treeString);
+		struct Node* root = buildTree(treeString);
+		Solution ob;
+		cout << ob.dupSub(root) << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
